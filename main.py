a102cb5e4874478acc43cbe8f40549e14ce5a25b
Artem-Shabaltun <134101240+Artem-Shabaltun@users.noreply.github.com>
2023-08-20 23:03:39 +0300

    Hw_11_assist_bot_v.1.0.1

--- main.py
+++ main.py
@@ -35,10 +35,16 @@ class Name(Field):
         super().__init__(value)
 
 
+class Email(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+
 class Record:
     def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
+        self.emails = []
         self.birthday = birthday
 
     def add_phone(self, number: Phone):
@@ -54,6 +60,20 @@ class Record:
         for num in self.phones:
             if num == value:
                 self.phones.remove(value)
+
+    def add_email(self, email: Email):
+        email_address = Email(email)
+        if email_address not in self.emails:
+            self.emails.append(email_address)
+
+    def update_email(self, old_email, new_email):
+        index = self.emails.index(old_email)
+        self.emails[index] = new_email
+
+    def delete_email(self, value):
+        for email in self.emails:
+            if email == value:
+                self.emails.remove(value)
 
     def calculate_days_to_birthday(self):
         if self.birthday:
@@ -91,22 +111,38 @@ address_book = ADDRESSBOOK()
 
 record1 = Record("John")
 phone1 = Phone("123456789")
+email1 = Email("john@example.com")
 record1.add_phone(phone1)
+record1.add_email(email1)
 birthday1 = Birthday(datetime(1990, 5, 15))
 record1.birthday = birthday1
 
 record2 = Record("Jane")
 phone2 = Phone("987654321")
+email2 = Email("jane@example.com")
 record2.add_phone(phone2)
+record2.add_email(email2)
 birthday2 = Birthday(datetime(1985, 8, 29))
 record2.birthday = birthday2
 
+record3 = Record("Alice")
+phone3 = Phone("555555555")
+email3 = Email("alice@example.com")
+birthday3 = Birthday(datetime(1995, 10, 20))
+record3.add_phone(phone3)
+record3.add_email(email3)
+record3.birthday = birthday3
+address_book.add_record(record3)
+
+
 address_book.add_record(record1)
 address_book.add_record(record2)
+address_book.add_record(record3)
 
 for record in address_book:
     print("Name:", record.name.value)
     print("Phones:", record.phones)
+    print("Emails:", [email.value for email in record.emails])
     print("Birthday:", record.birthday.value if record.birthday else "N/A")
     print("Days to Birthday:", record.calculate_days_to_birthday())
     print()
